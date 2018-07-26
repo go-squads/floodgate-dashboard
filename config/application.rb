6f6ab7cc440273cc8269033f1fe45f243f930342
Vincent B <vincent.bensawan@aux.go-jek.com>
2018-07-26 15:51:34 +0700

    Implemented function to connect to database

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module FloodgateDashboard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.database_name = "analyticsKafkaDB"
+    config.user_name = ""
+    config.password = ""
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
