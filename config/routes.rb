000103aabf864974da269438ac0958b5e24259e8
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-08-19 00:24:44 +0700

    add route to dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
-  
-  get ':measurement/fieldset' => 'home#get_fieldset'
 
-  get 'home/visualize'
-  get 'home/forecast'
+  get ':topic' => 'dashboard#show'
+  # get ':measurement/fieldset' => 'dashboard#show'
+
+  # get 'home/visualize'
+  # get 'home/forecast'
 end
