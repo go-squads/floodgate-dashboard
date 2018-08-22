c1f452ff79d42c340ec656b7233ae821326787e8
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-08-22 17:00:52 +0700

    Add chart.js controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   def connect_to_db
-   if !@db
      db_address = ENV["MONGO_ADDRESS"]
      print("Connecting to MongoDB with: #{db_address}" )
      client = Mongo::Client.new(db_address)
      @db = client.use(ENV["MONGO_NAME"]).database
-   end
   end
 end
