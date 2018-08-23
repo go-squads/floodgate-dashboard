f4ecfd1a5e41c657b0074b7ff921c75fcd9b4c58
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-08-23 08:02:59 +0700

    Add time precision rule to query

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,13 @@ class DashboardController < ApplicationController
   require 'mongo'
   before_action :connect_to_db
   skip_before_action :verify_authenticity_token
-
+  TIMESTAMP_PRECISION= {
+      'minute' => '%Y-%m-%dT%H:%M:00Z',
+      'hour' => '%Y-%m-%dT%H:00:00Z',
+      'day' => '%Y-%m-%d',
+      'month' => '%Y-%m-01',
+      'year' => '%Y'
+  }.freeze
   def index
 
   end
@@ -17,7 +23,7 @@ class DashboardController < ApplicationController
   end
 
   def match_rule(filter, bound)
-    puts ("Building filter with: #{filter}")
+    puts("Building filter with: #{filter}")
     rule = {}
     filter.each do |k,v|
       if v.length > 0
@@ -34,7 +40,10 @@ class DashboardController < ApplicationController
     @collection = @db[collection_name]
     match_rule= match_rule(JSON.parse(params[:filter]), params[:bound])
     puts("Rule: #{match_rule.to_json}")
-    @data = @collection.aggregate([{'$match'=> match_rule},{'$group' => {_id: '$timestamp', count: {'$sum' => '$count'}}}])
+    time_format = TIMESTAMP_PRECISION[params['precision']]
+    puts("Precision Format: #{params['precision']} => #{time_format}")
+    group_rule =  {_id: {'$dateToString'=> {format: time_format, date: {'$dateFromString' => {dateString: '$timestamp'}}}}, count: {'$sum' => '$count'}}
+    @data = @collection.aggregate([{'$match'=> match_rule},{'$group' => group_rule}, {'$sort'=> {'_id': 1}}])
     print("\n\n\n")
   end
 end
